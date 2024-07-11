cf4d133c9726618a973a8020c32d5c9337c01b64
Manar Ben salah <mben-sal@e3r3p12.1337.ma>
2024-07-11 19:34:19 +0100

    corricte exam

--- cpp_module00/Warlock.hpp
+++ cpp_module00/Warlock.hpp
@@ -1,24 +1,17 @@
 #pragma once
 
-#include <iostream>
+#include <iostream>	
 
 class Warlock
 {
-    private:
-        std::string name;
-        std::string title;
-
-        Warlock();
-        Warlock(Warlock const &other);
-        Warlock &operator=(Warlock const &other);
-    public:
-        Warlock(std::string const &name, std::string const &title);
-        ~Warlock();
-
-        std::string const &getName() const;
-        std::string const &getTitle() const;
-
-        void setTitle(std::string const &title);
-
-        void introduce() const;
+	private:
+		std::string name;
+		std::string title;
+	public:
+		Warlock(std::string const &name, std::string const &title);
+		~Warlock();
+		std::string const &getName() const;
+		std::string const &getTitle() const;
+		void setTitle(std::string const &title);
+		void introduce() const;
 };
